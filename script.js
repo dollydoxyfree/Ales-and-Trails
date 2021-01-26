30a41c3f6e068050d9b82d8bc08a09d5ebaf11dc
Jessica Embury <juannajane@Jessicas-MacBook-Air.local>
2021-01-26 12:34:26 -0800

    bare bones, begging api connectons

---
+++ script.js
@@ -0,0 +1,17 @@
+// Async/Await w/ Try/Catch
+// API docs  http://api.brewerydb.com/v2/{endpoint}/?key=be58abef0327aba1aa703a78ffa031fd
+
+
+
+async function fetchData(brewery) {
+
+  const url = `http://api.brewerydb.com/v2/${locality}/?key=be58abef0327aba1aa703a78ffa031fd`
+  try {
+    let res = await axios.get(url)
+    console.log(res.data[0])
+    const data = res.data[0]
+    removeBrewery()
+    showBreweryData(data)
+  } catch (err) {
+      console.log(err)
+}
