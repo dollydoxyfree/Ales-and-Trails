93a3720765afd594c6ee4f40e317adc47c8f1650
Jessica Embury <juannajane@Jessicas-MacBook-Air.local>
2021-01-26 12:59:02 -0800

    fixing script connection

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 async function fetchData(brewery) {
 
-  const url = `http://api.brewerydb.com/v2/${locality}/?key=be58abef0327aba1aa703a78ffa031fd`
+  const url = `http://api.brewerydb.com/v2/brewery/KRB0Bo/${location}/?key=be58abef0327aba1aa703a78ffa031fd`
   try {
     let res = await axios.get(url)
     console.log(res.data[0])
@@ -13,5 +13,6 @@ async function fetchData(brewery) {
     removeBrewery()
     showBreweryData(data)
   } catch (err) {
-      console.log(err)
+    console.log(err)
+  }
 }
