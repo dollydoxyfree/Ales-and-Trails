02b4e7d80c54a789becefe6b52e8dac9d90c3ae9
Jessica Embury <juannajane@Jessicas-MacBook-Air.local>
2021-01-27 12:13:12 -0800

    console log results

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
 // API docs = http://api.brewerydb.com/v2/{endpoint}/?key=be58abef0327aba1aa703a78ffa031fd
+
 const url = 'https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/locations/?key=be58abef0327aba1aa703a78ffa031fd'
-
-
-
-
-
 
 axios.get(url)
   .then((res) => {
-  console.log(res)
+    const results = res.data.data
+    console.log(results)
 })
   .catch((err) => {
   console.log(err)
-})
+  })
 
-// const getBrewery = async () => {
-//   // async function fetchData(brewery) {
 
-//   try {
-//     let res = await axios.get(url)
-//     console.log(res.data.message)
-//     // let endpont = body.querySelector('#brewery-search').value
-//     let brewerySearch = Object.keys(res.data.message)
-   
-//     console.log(brewerySearch)
-//     } catch (err) {
-//       console.log(err)
-//     }
-//   }
 
-// getBrewery()
+const getBrewery = async () => {
+  try {
+    let res = await axios.get(url)
+    // console.log(res.data.data[27])
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+getBrewery()
+
+
 
 // function showBreweryData(data) {
 //   console.log(data)
@@ -43,16 +38,18 @@ axios.get(url)
 //     <p>  Notes: ${data.hoursOfOperationNotes}
 //     <p> Brewery Type: ${data.locationType}`
 
-//   removeBrewery()
+//   // removeBrewery()
 
 //   console.log(breweryInfo)
 //   let dataContainer = document.querySelector('#brewery-data')
 //   dataContainer.insertAdjacentHTML('afterend', breweryInfo)
 //   return breweryInfo
 
-//   removeBrewery()
+//   // removeBrewery()
 
 // }
+
+// showBreweryData()
 
 // const form = document.querySelector('form')
 // console.log(form)
