93d484e9b06db4cc8dd0110dba0dbb805c7ba8c9
Jessica Embury <juannajane@Jessicas-MacBook-Air.local>
2021-01-27 16:38:08 -0800

    clean up JS

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // API docs = http://api.brewerydb.com/v2/{endpoint}/?key=be58abef0327aba1aa703a78ffa031fd
 
 
-const getBreweryData= () => {
+const getBreweryData = () => {
   const url = 'https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/locations/?key=be58abef0327aba1aa703a78ffa031fd'
 
   axios.get(url)
     .then((res) => {
       const results = res.data.data
-      results.splice(42,1)
-     
+      results.splice(42, 1)
+
       // console.log(results)
+      removePrevious()
 
       results.forEach((brew) => {
         // console.log(brew)
@@ -42,6 +43,7 @@ const getBreweryData= () => {
         image.setAttribute('alt', 'brewery-logo')
         brewDiv.append(image)
 
+        // removePrevious()
 
       })
     })
@@ -49,68 +51,19 @@ const getBreweryData= () => {
       console.log(err)
     })
 }
-getBreweryData()
-
-// const getBrewery = async () => {
-//   try {
-//     let res = await axios.get(url)
-//     // console.log(res.data.data[27])
-//     } catch (err) {
-//       console.log(err)
-//     }
-//   }
-
-// getBrewery()
 
 
+const button = document.querySelector('button')
+// console.log(button)
+button.addEventListener('click', getBreweryData)
 
-// function showBreweryData(data) {
-//   console.log(data)
-//   const breweryInfo = `
-//     <h1> Brewery Name: ${data.name}
-//     <h2> Address: ${data.streetAddress}
-//     <h2> City: ${data.locality}
-//     <h2> State: ${data.region} 
-//     <h4> Website: ${data.website}
-//     <p>  Hours: ${data.hoursOfOperationExplicitString}
-//     <p>  Notes: ${data.hoursOfOperationNotes}
-//     <p> Brewery Type: ${data.locationType}`
+function removePrevious() {
+  const removeSearch = document.querySelector('.brew-data')
+  console.log(removeSearch.lastChild)
+  while (removeSearch.lastChild) {
+    removeSearch.removeChild(removeSearch.lastChild)
+  }
+}
 
-//   // removeBrewery()
+removePrevious()
 
-//   console.log(breweryInfo)
-//   let dataContainer = document.querySelector('#brewery-data')
-//   dataContainer.insertAdjacentHTML('afterend', breweryInfo)
-//   return breweryInfo
-
-//   // removeBrewery()
-
-// }
-
-// showBreweryData()
-
-// const form = document.querySelector('form')
-// console.log(form)
-// form.addEventListener('search', (e) => {
-//   e.preventDefault()
-
-//   removeBrewery()
-
-//   const inputValue = document.querySelector('#brewery-search').value
-//   console.log(inputValue)
-//   fetchData(inputValue)
-// })
-
-
-
-// removeBrewery()
-
-// function removeBrewery() {
-//   const breweryContainer = document.querySelector('#get-brew')
-//   while (breweryContainer.lastChild) {
-//     breweryContainer.removeChild(breweryContainer.lastChild)
-//   }
-// }
-
-
-// removeBrewery()
