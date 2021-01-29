cb7a3fac55a9815831aea4d735712aab5f707720
Jessica Embury <juannajane@Jessicas-MacBook-Air.local>
2021-01-29 12:03:34 -0800

    switch API

--- script.js
+++ script.js
@@ -1,70 +1,57 @@
-// API docs = http://api.brewerydb.com/v2/{endpoint}/?key=be58abef0327aba1aa703a78ffa031fd
-
-// `https://api.openbrewerydb.org/breweries/search?query=${search}`
+// API URL `https://api.openbrewerydb.org/breweries/search?query=${inputValue}`
 
 
-function getBreweryData () {
-  const url = 'https://cors-anywhere.herokuapp.com/https://api.brewerydb.com/v2/locations/?key=be58abef0327aba1aa703a78ffa031fd'
-
-  axios.get(url)
-    .then((res) => {
-      const results = res.data.data
-
-
-      console.log(results)
-      removePrevious()
-
-       results.forEach((brew) => {
-        // console.log(brew)
-
-        const dataDiv = document.querySelector('.brew-data')
-        // console.log(dataDiv)
-        const brewDiv = document.createElement('div')
-        dataDiv.append(brewDiv)
-
-        // const breweryName = brew.brewery.name
-        // const website = brew.brewery.website
-        // const description = brew.brewery.description
-        // const type = brew.locationTypeDisplay
-        // const established = brew.brewery.established
-        // const street = brew.streetAddress
-        // const city = brew.locality
-        // const state = brew.region
-        // const zip = brew.postalCode
-        // const address = street + city + state + zip
-
-        const breweryInfo = document.createElement('div')
-
-        breweryInfo.innerHTML =
-
-          `
-        <h1>Brewery: ${brew.brewery.name} </h1>
-        <h3>Website:${brew.brewery.website}</h3>
-        <p>Description: ${brew.brewery.description} </p>
-        <h6>Type: ${brew.locationTypeDisplay}</h6> 
-        <h5> ${brew.brewery.established} </h5>
-        <h3>Address: ${brew.streetAddress} ${brew.locality} ${brew.region} ${brew.postalCode} </h3> `
-
-        brewDiv.append(breweryInfo)
-
-        const logo = brew.brewery.images.squareMedium
-
-        const image = document.createElement('img')
-
-        image.setAttribute('src', logo)
-        image.setAttribute('alt', 'brewery-logo')
-        brewDiv.append(image)
-
-        // removePrevious()
-        return breweryInfo
-
-      })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+const getData = async (inputValue) => {
+  const url = `https://api.openbrewerydb.org/breweries/search?query=${inputValue}`
+  
+  try {
+    let res = await axios.get(url)
+    console.log(res.data)
+    const data = res.data
+    // console.log(data)
+    // getBrewery(data)
+    // getBreweryInfo(data)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
+// getData()
+
+function getBreweryInfo(breweries) {
+  // data.forEach((brew) => {
+  //   let data = url.data.data
+  //   console.log(data)
+
+
+//     const dataDiv = document.querySelector('.brew-data')
+//     // console.log(dataDiv)
+//     const brewDiv = document.createElement('div')
+//     dataDiv.append(brewDiv)
+
+
+//     const breweryInfo = document.createElement('div')
+
+//     breweryInfo.innerHTML.forEach = `
+//   <h1>Name: ${Object.name}</h1>`
+//     console.log(breweryInfo)
+//   brewDiv.append(breweryInfo)
+//   return breweryInfo
+  
+  breweries.forEach((brew) => {
+  console.log(brew)
+
+    
+
+
+    
+    
+})
+
+  
+}
+
+// getBreweryInfo()
 
 
 
@@ -76,9 +63,9 @@ form.addEventListener('submit', (e) => {
   removePrevious()
   const inputValue = document.querySelector('#input-search').value
   console.log(inputValue)
-  // getBreweryData(inputValue)
-  getBreweryData(inputValue)
-  
+  // getBreweryInfo(inputValue)
+
+  getData(inputValue)
 })
 
 
@@ -94,3 +81,16 @@ function removePrevious() {
 }
 
 removePrevious()
+
+
+// const getBrewery = (brew) => {
+//   console.log(brew)
+//   brew.forEach(brewery => {
+//     let inputValue = document.querySelector('#get-brewery').value
+//     console.log(inputValue)
+    
+//   })
+// }
+
+// getBrewery()
+
